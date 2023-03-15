c0274dfae1c2cefa9f4c37e00884e0ff5e787a81
vvdziuba <vdziuba@n-ix.com>
2023-03-15 15:37:56 +0200

    implemented add, delete, like reducers

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,107 @@
-import React, { useEffect } from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import styled from "styled-components";
 import "./App.css";
-import {  useDispatch } from "react-redux"; 
-import { set, ItemType } from "./store/items";
+import { useDispatch } from "react-redux";
+import { set, deleteItem, like } from "./store/items";
+import { Button } from "@mui/material";
+import { store } from "./store/store";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import Divider from "@mui/material/Divider";
+
+const StyledContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 100px;
+  flex-direction: column;
+  align-items: center;
+`;
 
 function App() {
-
   const dispatch = useDispatch();
+  const state = store.getState()?.items;
 
-  useEffect(()=>{
-   // @ts-ignore
-   dispatch(set([{id:1, title: 'some title'}] as ItemType[]))
-  },[]);
+  const [data, setData] = useState({ value: [] });
+
+  const handleDelete = (id: any) => {
+    dispatch(deleteItem(id));
+    // @ts-ignore
+    setData(state);
+  };
+
+  const addItem = () => {
+    dispatch(
+      set({
+        // @ts-ignore
+        id: state.value.length + 1,
+        likes: 0,
+        // @ts-ignore
+        title: `item number${state.value.length + 1}`,
+      })
+    );
+    // @ts-ignore
+    setData(state);
+  };
+
+  const likeItem = (id: number) => {
+    // @ts-ignore
+    dispatch(like(id));
+    // @ts-ignore
+    setData(state);
+  };
 
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    <StyledContainer>
+      <Button variant="contained" onClick={addItem}>
+        Add Item
+      </Button>
+      <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        {(state.value || data.value).map((value, ind) => {
+          const labelId = `checkbox-list-label-${value}`;
+          return (
+            <>
+              <ListItem
+                key={ind}
+                secondaryAction={
+                  <>
+                    <IconButton
+                      edge="end"
+                      aria-label="delete"
+                      onClick={() => handleDelete(value.id)}
+                    >
+                      <DeleteIcon />
+                    </IconButton>
+                    <IconButton
+                      edge="end"
+                      aria-label="delete"
+                      onClick={() => likeItem(value.id)}
+                    >
+                      <ThumbUpIcon />
+                    </IconButton>
+                  </>
+                }
+                disablePadding
+              >
+                <ListItemButton role={undefined} dense>
+                  <ListItemIcon />
+                  <ListItemText
+                    id={labelId}
+                    primary={`${value.title} has ${value.likes} likes`}
+                  />
+                </ListItemButton>
+              </ListItem>
+              <Divider light={false} />
+            </>
+          );
+        })}
+      </List>
+    </StyledContainer>
   );
 }
 
--- src/store/items.ts
+++ src/store/items.ts
@@ -1,36 +1,50 @@
-import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
-import { RootState } from './store'
-
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "./store";
 
 export type ItemType = {
-    title: string,
-    id: string
-}
+  title: string;
+  id: number;
+  likes: number;
+};
 
 export type ItemsType = {
-    value: ItemType[]
-}
+  value: ItemType[];
+};
 
 // Define the initial state using that type
 const initialState: ItemsType = {
+  // @ts-ignore
   value: [],
-}
+};
 
 export const counterSlice = createSlice({
-    name: 'items',
-    // `createSlice` will infer the state type from the `initialState` argument
-    initialState,
-    reducers: {
-      set: (state, action: PayloadAction<ItemsType>) => {
-        // @ts-ignore
-        state.value = action.payload
-      },
+  name: "items",
+  // `createSlice` will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    set: (state, action: PayloadAction<ItemsType>) => {
+      // @ts-ignore
+      state.value.push(action.payload);
     },
-  })
+    deleteItem: (state, action: PayloadAction<ItemsType>) => {
+      // @ts-ignore
+      state.value = state.value.filter((item) => item.id !== action.payload);
+    },
+    like: (state, action: PayloadAction<ItemsType>) => {
+      const indexes = state.value.findIndex(
+        // @ts-ignore
+        (item) => item.id === action.payload
+      );
+      const allElements = [...state.value];
+      allElements[indexes] = {
+        ...allElements[indexes],
+        likes: allElements[indexes].likes++,
+      };
+    },
+  },
+});
+export const { set, deleteItem, like } = counterSlice.actions;
+export const selectCount = (state: RootState) => state.items.value;
 
-  export const { set } = counterSlice.actions;
-
-  export const selectCount = (state: RootState) => state.items.value
-
-export default counterSlice.reducer
+export default counterSlice.reducer;
